3af1c8aed29baf8942af208f646891364c4631ce
krben <discgolfkristian@gmail.com>
2020-02-15 13:06:16 +0100

    logger class, logging to file

---
+++ logger.py
@@ -0,0 +1,36 @@
+import logging
+
+class Logger(object):
+
+  def __init__(self, name='logger', level=logging.DEBUG):
+    
+    self.logger = logging.getLogger(name)
+    self.logger.setLevel(level)
+
+    fh = logging.FileHandler('%s.log' % name, 'w')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    self.logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    self.logger.addHandler(sh)
+
+  def debug(self, msg):
+      self.logger.debug(msg)
+
+  def info(self, msg):
+      self.logger.info(msg)
+
+  def warning(self, msg):
+      self.logger.warning(msg)
+
+  def error(self, msg):
+      self.logger.error(msg)
+
+
+if __name__ == '__main__':
+  log = Logger()
+  log.debug('debug')
+  log.info('info')
+  log.warning('warning')
+  log.error('error')
